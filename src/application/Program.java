d0ddfa74f464f7cdadf5910892df83e914fffae7
Lucas Santos <43357885+lucasantos603@users.noreply.github.com>
2020-04-30 11:46:29 -0300

    1.0.2
    Addiction "src\application\Program.java"

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import model.entidades.Department;
+import model.entidades.Seller;
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Book");
+		Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
+
+	}
+
+}
